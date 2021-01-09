def204a3295bf0ddb479d52e85b21d1ec83504ff
mayankrichu <71438205+mayankrichu@users.noreply.github.com>
2021-01-09 18:13:53 +0100

    Tweets based on HashedTags

---
+++ Tweetsmention.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import twint
+import nest_asyncio
+nest_asyncio.apply()
+
+hashtags= ["#CrackerBan", "#BloodLessEid", "intolerant india", "tolerant india", "islamophobia in india", "hindhuphobia in india"]
+
+for i in hashtags:
+    output_file= f'/Users/mayank/Documents/Online Political Polarization/Tweets/{i}.csv'
+
+    config = twint.Config()
+    config.Search= f'{i}'
+    config.Lang = "en"
+    config.Resume = output_file
+    config.Since = str(datetime(2015,1,14))
+    config.Until = str(datetime(2020,1,14))
+    config.Store_csv= True
+    config.Output = output_file
+    twint.run.Search(config)
