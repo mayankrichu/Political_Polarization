f2a72a8c71ee47483510bb831b33c8f94d831708
mayankrichu <71438205+mayankrichu@users.noreply.github.com>
2021-01-09 19:23:20 +0100

    number of counts

--- mention counts.py
+++ mention counts.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import re
-import numpy as np
+
 persons= ['DeShobhaa','mynameswatik','drusawasthi','netshrink','SattarFarooqui','Kaviraj_AFC','ipsvipul_','KPCentralDiv','UmmeSumbulaZuha','FaaridZaeem','sairamogirala', 'parimalaloke', 'libertariandesi', 'Sanjay_Dixit', 'RiseofBurnol', 'TheSkandar','IAnnapurnna','MahaveerVJ','shruttitandon','Sweet_Honeygal' ]
 
 
@@ -8,9 +8,8 @@ def count_mention(file):
     x=[]
     table=[]
     wordcount=[]
-    for i in range(0, len(tweets)):
-        z= tweets.iloc[i]
-
+    for i in range(0, len(persons)):
+        z= persons.iloc[i]
         match=re.findall(r"(@[a-zA-Z0-9]*)\s", z[0])
         for j in match:
             if j in table:
@@ -26,7 +25,7 @@ def count_mention(file):
 
 dataframe_list=[]
 for i in range(0,len(persons)):
-    tweets = pd.read_csv(f'/Users/mayank/Desktop/Tweets/{persons[i]}.csv', sep='delimiter', header=None)
+    tweets = pd.read_csv(f'/Users/mayank/Desktop/Tweets/{persons[i]}.csv')
     p = count_mention(tweets)
     df=pd.DataFrame(p)
     df.to_csv(f'/Users/mayank/Desktop/Tweets/mentions/{persons[i]}.csv')
